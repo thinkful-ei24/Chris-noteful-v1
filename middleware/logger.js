a8cc4a51b6fea9dfd30a641b1e332c9b4b71e93c
Chris Rudder <cbrudder84@gmail.com>
2018-09-05 16:15:39 -0400

    added error handling 404 and 500

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,7 +2,7 @@
 
 function logger(req, res, next){
   const now = new Date();
-  console.log(`${now.toLocalDateString()}, ${req.method}, ${req.url}`);
+  console.log(`${now.toLocaleString()} ${req.method} ${req.url}`);
   next();
 }
 
